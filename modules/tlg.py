ddf8815d5b83817438448ad7cbb3ef6e4f13d8cb
pavmil <71142376+pavmil@users.noreply.github.com>
2023-02-19 19:22:36 +0300

    Update tlg.py

--- modules/tlg.py
+++ modules/tlg.py
@@ -2,5 +2,8 @@ import telebot
 
 
 def send_notify(message):
-    bot = telebot.TeleBot("6263162578:AAGuGsiKF7_WoOiZ4dptCqry-ZkishL6w1o") # your api hash
-    bot.send_message(423979863, message) # your chat id
+    chat_id = "" # your api hash
+    api_hash =  0# your chat id
+    
+    bot = telebot.TeleBot(api_hash) 
+    bot.send_message(chat_id, message)
